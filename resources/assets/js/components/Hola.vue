f244d2524589734685189359097e6e87f6de900c
Jon Clausen <jon_clausen@silowebworks.com>
2019-05-02 13:57:09 -0400

    convert to Webpack ^4, Bootstrap ^4, Elixir ^3

--- resources/assets/js/components/Hola.vue
+++ resources/assets/js/components/Hola.vue
@@ -1,11 +1,9 @@
 <template>
     <div>
         <h1>Hola, {{ name }}!</h1>
-
         <input type="text" v-model="name">
     </div>
 </template>
-
 <script>
     export default {
         data() {
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const elixir = require( "coldbox-elixir" );
+const webpack = require( "webpack" );
+
+elixir.config.mergeConfig({
+    plugins: [
+        // globally scoped items which need to be available in all templates
+        new webpack.ProvidePlugin({
+			$              : "jquery",
+			jQuery         : "jquery",
+			"window.jQuery": "jquery",
+			"window.$"     : "jquery",
+            "Vue"          : ["vue/dist/vue.esm.js", "default"],
+            "window.Vue"   : ["vue/dist/vue.esm.js", "default"]
+        })
+    ]
+});
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your ColdBox application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+module.exports = elixir(function(mix) {
+	// Mix App styles
+	mix.sass( "app.scss" )
+		// Mix JS and VueJS components
+		.vue( "app.js" );
+} );
